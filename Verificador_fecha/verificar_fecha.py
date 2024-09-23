815d5047828b5ad14d808e02c7d4410c1e629289
SM0NKY <smonky123.9.ki@hotmail.com>
2024-09-22 22:34:43 -0700

    Week Calculator

--- Verificador_fecha/verificar_fecha.py
+++ Verificador_fecha/verificar_fecha.py
@@ -13,10 +13,10 @@ class Carpeta():
     [2] -> Date: `int`
     """
     def __init__(self,*inicio_ciclo:List[int]) -> None:
-        self.fecha_actual:Tuple[vars] = time.localtime()
+        self.fecha_actual_año:Tuple[vars] = time.localtime().tm_yday
         self.inicio_del_ciclo:Tuple[int] = [x for x in inicio_ciclo]
     
-    def calcular_semanas(self) -> None:
+    def calcular_semanas(self) -> int:
         """ It calculates the quantity of weeks that passed since the start of the year cicle
         
         Parameters
@@ -26,8 +26,11 @@ class Carpeta():
         :returns `None`:
         
         """
-        print(self.fecha_actual[0:3])
-        print(self.inicio_del_ciclo)
+
+        dia_del_año:int = time.strptime(f"{self.inicio_del_ciclo[0]}-{self.inicio_del_ciclo[1]}-{self.inicio_del_ciclo[2]}","%Y-%m-%d").tm_yday
+        sem_clases:int = (self.fecha_actual_año - dia_del_año)//7 # In this line I utilized the `//` double slash to divide and round down to the nearest integer
+        print(f"Las semanas de clase son las siguientes: \n{sem_clases} Semanas")
+        return sem_clases
     
 # The structure of the tuple of the localtime
 # [0] -> tm_year
@@ -43,5 +46,5 @@ class Carpeta():
 # Pruebas para corroborar los metodos de la clase
 if __name__ == "__main__":
     
-    objeto:object = Carpeta(2024,3,21)
+    objeto:object = Carpeta(2024,7,1)
     objeto.calcular_semanas()
