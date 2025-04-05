84fe7e46c60a8c554cd36055eb7c509d7ea2cf3d
SM0NKY <smonky123.9.ki@hotmail.com>
2025-04-04 21:44:45 -0700

    Configuracion

--- Main_W/config.py
+++ Main_W/config.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 from typing import Any, List, Literal, Dict,Callable, Optional
 import os
+from pathlib import Path
+import json
 
 class Einstellungen(ctk.CTkToplevel):
     """ This method is in charge of the settings of the main window
@@ -9,27 +11,35 @@ class Einstellungen(ctk.CTkToplevel):
     ----------
     `master` : Optional[object] 
         This method utilizes herence to build up the windows layout, it is recomended to use self
+    
+    ``    
     """
     
-    def __init__(self, master:Optional[object] = None):
+    def __init__(self,master:Optional[object] = None):
         super().__init__(master)
         self.geometry("400x500")
         self.title("Configuration")
         self.protocol("WM_DELETE_WINDOW",lambda:None)
-        
+        #Variable de parametro
+        self.showed:bool = False
+
+        #Directorio .json
+        self.directorio:str = os.path.join(Path(__file__).parent,"config.json")
 
         #Agregar los botones y el boton para seleccionar el directorio
         self.label1:object|ctk.CTkLabel = ctk.CTkLabel(master = self,text="El directorio seleccionado es:", font=("Sans Seriff", 16))
         self.label2:object|ctk.CTkLabel = ctk.CTkLabel(master= self, text= "", font= ("Sans Seriff", 16))
         self.button1:object|ctk.CTkButton = ctk.CTkButton(master= self, text="Cambiar directorio directorio", font=("Sans Seriff", 16), command= self.directory)
-    
-    def exceptions(message:Literal["Error al inicializar la ventana","Error al buscar el directorio"]) -> Any:
+        self.button2:object|ctk.CTkButton = ctk.CTkButton(master= self, text=" Regresar", font=("Sans Seriff",16), command= self.abholen)
+
+    def exceptions(message:Literal["Error al inicializar la ventana","Error al buscar el directorio","Error al minimizar la ventana"]) -> Any:
         def deco(func:Callable[...,Any]) -> Any:
             def wr(*args,**kwargs) -> Callable[...,Any]:
                 try:
                     return func(*args,**kwargs)
                 except Exception as e:
                     print(f"{message}:{e}, en la función {func.__name__}")
+                    raise e
             return wr
         return deco
 
@@ -39,12 +49,18 @@ class Einstellungen(ctk.CTkToplevel):
 
         Parameters
         ----------
-        `None`
+        `showed` :bool
+            This pointer/parameter helps the mainwindow avoid errors with another window 
         """
-
+        #Cambia la variable correspondiente para indicar si esta abierta la ventana
+        self.showed:bool = True
+        
+        #Muestra los objetos creados de la ventana
+        self.button2.pack(padx =10, pady = 10,anchor = "nw")
         self.label1.pack(padx = 10, pady = 10)
         self.label2.pack(padx = 10, pady = 10)
         self.button1.pack(padx = 10, pady = 10)
+        self.load_dir()
         
         self.deiconify()
         
@@ -58,9 +74,43 @@ class Einstellungen(ctk.CTkToplevel):
         """
         selected_dir:str = os.path.abspath(ctk.filedialog.askdirectory())
         self.label2.configure(text = selected_dir)
+        #Se revisa si el directorio existe, en caso de que exista
+        with open(self.directorio, 'r+') as directorio:
+            json.load(directorio)
 
+        with open(self.directorio,'w') as directory:
+            
+            if directory:
+                json.dump({"directory":selected_dir}, directory, indent=4)
 
+    @exceptions("Error al inicializar la ventana")            
+    def load_dir(self) -> None:
+        """ Loads the current directory into the label of the window
+
+        Parameters
+        ----------
+        `None`
+        """
+        #Carga el nuevo directorio y lo muestra en la ventana
+        with open(self.directorio, 'r') as dir:
+            directory:Dict[str,str] = json.load(dir)
+            print(directory)
+            self.label2.configure(text = directory.get("directory"))
+
+    @exceptions("Error al minimizar la ventana")
+    def abholen(self) -> None:
+        """ This method withdraws the window from the screen
+
+        Parameters
+        ----------
+        `showed` : bool
+            Uses a variable to indicate if there's another window that is in use to avoid errorss
+        """
+        self.showed:bool = False
+        self.withdraw()
+        
 if  __name__ == "__main__":
+
     settings:object|Einstellungen = Einstellungen()
     settings.show_w()
     settings.mainloop()
--- Main_W/main_window.py
+++ Main_W/main_window.py
@@ -1,8 +1,9 @@
+from tkinter import messagebox
 import customtkinter as ctk 
 from typing import Literal,Any, Callable, List,Dict
 
 #Definir la ventana principal
-
+from config import Einstellungen
 class Main_Window(ctk.CTk):
     """ This class starts the main window
     
@@ -17,13 +18,14 @@ class Main_Window(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Organizar")
-        self.geometry("300x300")
-        self.second_window = True
+        self.geometry("400x400")
 
+        self.settings = Einstellungen(master= self)
+        self.settings.abholen()
 
         #Aqui se agregan los objetos correspondientes a la ventana
-        self.label1:object|ctk.CTkLabel = ctk.CTkLabel(master= self, text= "Para organizar los archivos, confirma tu directorio en la configuración")
-
+        self.label1:object|ctk.CTkLabel = ctk.CTkLabel(master= self, text= "Para organizar los archivos, confirma tu directorio en la configuración",font= ("Sans Seriff", 16))
+        self.button1:object|ctk.CTkButton = ctk.CTkButton(master= self, text="Configuración", font=("Sans Seriff", 16), command= self.open_s)
 
     def exceptions(message:Literal["Error al mostrar la ventana",""]) -> None:
         def deco(func:Callable[...,Any]) -> Any:
@@ -32,18 +34,36 @@ class Main_Window(ctk.CTk):
                     return func(*args, **kwargs)
                 except Exception as e:
                     print(f"{message}: {e}, en la funcion {func.__name__}")
+                    raise e
             return wr
         return deco
 
+    def exceptandwind(message:Literal["Se tiene otra ventana abierta"]) -> None:
+        def deco(func:Callable[...,Any]) -> Callable[...,Any]:
+            def wr(self,*args,**kwargs) -> Any:
+                try:
+                    if self.settings.showed:
+                        return func(self,*args,**kwargs)
+                    else:
+                        messagebox.askokcancel("Ventana Abierta", text = "Porfavor cierra la ventana secundaria")
+                except Exception as e:
+                    print(f"{message}:{e}, en la funcion {func.__name__}")
+                    raise e
+            return wr
+        return deco
 
     @exceptions("Error al mostrar la ventana")
     def show_w(self) -> None:
-        # Aqui agregar los demas botones correspondientes
 
+        self.button1.pack(padx = 10, pady = 10, anchor = "nw")
         self.mainloop()
 
 
+    @exceptandwind("Se tiene otra ventana abierta")
+    def open_s(self) -> None:
+        self.settings.show_w()
 
 
 if __name__ == "__main__":
-    pass
+    window:Main_Window = Main_Window()
+    window.show_w()
